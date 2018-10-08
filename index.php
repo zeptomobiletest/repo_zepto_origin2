5120d77e31279d9a49ca165919d269f0c1cf3d81
tiffany law <ykltiffany@gmail.com>
2018-10-08 16:43:42 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Welcome World";
+
+
+echo "<br><br>Finally DONE !!!!!";
+
+echo "<br><br>GIT PUSH repo zepto3 auto deploy to live server";
+echo "<br><br>GIT PUSH repo zepto3 auto deploy to live server 2";
+
+echo "<br><br>GIT PUSH repo zepto3 control commit master";
+
+?>
