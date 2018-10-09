d11b39f2e8d5d1ad39552ad8d325c0f2723714c0
tiffany law <ykltiffany@gmail.com>
2018-10-09 10:39:14 +0800

    second commit

--- index.php
+++ index.php
@@ -5,9 +5,7 @@ echo "Welcome World";
 
 echo "<br><br>Finally DONE !!!!!";
 
-echo "<br><br>GIT PUSH repo zepto3 auto deploy to live server";
-echo "<br><br>GIT PUSH repo zepto3 auto deploy to live server 2";
+echo "<br><br>Finally DONE 2 !!!!!";
 
-echo "<br><br>GIT PUSH repo zepto3 control commit master";
 
 ?>
